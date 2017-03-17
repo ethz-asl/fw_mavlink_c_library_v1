0e292e5ea5ddf0533e2c840abfab050c120331a6
mantelt <thomas.mantel@mavt.ethz.ch>
2017-03-17 15:44:13 +0100

    generated headers for rev https://github.com/ethz-asl/fw_mavlink/tree/3d5f34490013f48cd068b8a415afd59bb40f8364

---
+++ ASLUAV/mavlink_msg_wind_sensor.h
@@ -0,0 +1,288 @@
+#pragma once
+// MESSAGE WIND_SENSOR PACKING
+
+#define MAVLINK_MSG_ID_WIND_SENSOR 213
+
+MAVPACKED(
+typedef struct __mavlink_wind_sensor_t {
+ float direction; /*<  Wind Direction*/
+ float avg_mag; /*<  Average wind measured*/
+ float max_mag; /*<  Maximum wind measured*/
+ float std_dev; /*<  Standard Deviation of measured wind magnitude*/
+}) mavlink_wind_sensor_t;
+
+#define MAVLINK_MSG_ID_WIND_SENSOR_LEN 16
+#define MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN 16
+#define MAVLINK_MSG_ID_213_LEN 16
+#define MAVLINK_MSG_ID_213_MIN_LEN 16
+
+#define MAVLINK_MSG_ID_WIND_SENSOR_CRC 173
+#define MAVLINK_MSG_ID_213_CRC 173
+
+
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_WIND_SENSOR { \
+    213, \
+    "WIND_SENSOR", \
+    4, \
+    {  { "direction", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_wind_sensor_t, direction) }, \
+         { "avg_mag", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_wind_sensor_t, avg_mag) }, \
+         { "max_mag", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_wind_sensor_t, max_mag) }, \
+         { "std_dev", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_wind_sensor_t, std_dev) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_WIND_SENSOR { \
+    "WIND_SENSOR", \
+    4, \
+    {  { "direction", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_wind_sensor_t, direction) }, \
+         { "avg_mag", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_wind_sensor_t, avg_mag) }, \
+         { "max_mag", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_wind_sensor_t, max_mag) }, \
+         { "std_dev", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_wind_sensor_t, std_dev) }, \
+         } \
+}
+#endif
+
+/**
+ * @brief Pack a wind_sensor message
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param direction  Wind Direction
+ * @param avg_mag  Average wind measured
+ * @param max_mag  Maximum wind measured
+ * @param std_dev  Standard Deviation of measured wind magnitude
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_wind_sensor_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               float direction, float avg_mag, float max_mag, float std_dev)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_WIND_SENSOR_LEN];
+    _mav_put_float(buf, 0, direction);
+    _mav_put_float(buf, 4, avg_mag);
+    _mav_put_float(buf, 8, max_mag);
+    _mav_put_float(buf, 12, std_dev);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_WIND_SENSOR_LEN);
+#else
+    mavlink_wind_sensor_t packet;
+    packet.direction = direction;
+    packet.avg_mag = avg_mag;
+    packet.max_mag = max_mag;
+    packet.std_dev = std_dev;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_WIND_SENSOR_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_WIND_SENSOR;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+}
+
+/**
+ * @brief Pack a wind_sensor message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param direction  Wind Direction
+ * @param avg_mag  Average wind measured
+ * @param max_mag  Maximum wind measured
+ * @param std_dev  Standard Deviation of measured wind magnitude
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_wind_sensor_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   float direction,float avg_mag,float max_mag,float std_dev)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_WIND_SENSOR_LEN];
+    _mav_put_float(buf, 0, direction);
+    _mav_put_float(buf, 4, avg_mag);
+    _mav_put_float(buf, 8, max_mag);
+    _mav_put_float(buf, 12, std_dev);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_WIND_SENSOR_LEN);
+#else
+    mavlink_wind_sensor_t packet;
+    packet.direction = direction;
+    packet.avg_mag = avg_mag;
+    packet.max_mag = max_mag;
+    packet.std_dev = std_dev;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_WIND_SENSOR_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_WIND_SENSOR;
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+}
+
+/**
+ * @brief Encode a wind_sensor struct
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ * @param wind_sensor C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_wind_sensor_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_wind_sensor_t* wind_sensor)
+{
+    return mavlink_msg_wind_sensor_pack(system_id, component_id, msg, wind_sensor->direction, wind_sensor->avg_mag, wind_sensor->max_mag, wind_sensor->std_dev);
+}
+
+/**
+ * @brief Encode a wind_sensor struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param wind_sensor C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_wind_sensor_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_wind_sensor_t* wind_sensor)
+{
+    return mavlink_msg_wind_sensor_pack_chan(system_id, component_id, chan, msg, wind_sensor->direction, wind_sensor->avg_mag, wind_sensor->max_mag, wind_sensor->std_dev);
+}
+
+/**
+ * @brief Send a wind_sensor message
+ * @param chan MAVLink channel to send the message
+ *
+ * @param direction  Wind Direction
+ * @param avg_mag  Average wind measured
+ * @param max_mag  Maximum wind measured
+ * @param std_dev  Standard Deviation of measured wind magnitude
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_wind_sensor_send(mavlink_channel_t chan, float direction, float avg_mag, float max_mag, float std_dev)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_WIND_SENSOR_LEN];
+    _mav_put_float(buf, 0, direction);
+    _mav_put_float(buf, 4, avg_mag);
+    _mav_put_float(buf, 8, max_mag);
+    _mav_put_float(buf, 12, std_dev);
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_WIND_SENSOR, buf, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+#else
+    mavlink_wind_sensor_t packet;
+    packet.direction = direction;
+    packet.avg_mag = avg_mag;
+    packet.max_mag = max_mag;
+    packet.std_dev = std_dev;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_WIND_SENSOR, (const char *)&packet, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+#endif
+}
+
+/**
+ * @brief Send a wind_sensor message
+ * @param chan MAVLink channel to send the message
+ * @param struct The MAVLink struct to serialize
+ */
+static inline void mavlink_msg_wind_sensor_send_struct(mavlink_channel_t chan, const mavlink_wind_sensor_t* wind_sensor)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    mavlink_msg_wind_sensor_send(chan, wind_sensor->direction, wind_sensor->avg_mag, wind_sensor->max_mag, wind_sensor->std_dev);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_WIND_SENSOR, (const char *)wind_sensor, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+#endif
+}
+
+#if MAVLINK_MSG_ID_WIND_SENSOR_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is the size of a full mavlink message. This
+  is usually the receive buffer for the channel, and allows a reply to an
+  incoming message with minimum stack space usage.
+ */
+static inline void mavlink_msg_wind_sensor_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  float direction, float avg_mag, float max_mag, float std_dev)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char *buf = (char *)msgbuf;
+    _mav_put_float(buf, 0, direction);
+    _mav_put_float(buf, 4, avg_mag);
+    _mav_put_float(buf, 8, max_mag);
+    _mav_put_float(buf, 12, std_dev);
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_WIND_SENSOR, buf, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+#else
+    mavlink_wind_sensor_t *packet = (mavlink_wind_sensor_t *)msgbuf;
+    packet->direction = direction;
+    packet->avg_mag = avg_mag;
+    packet->max_mag = max_mag;
+    packet->std_dev = std_dev;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_WIND_SENSOR, (const char *)packet, MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN, MAVLINK_MSG_ID_WIND_SENSOR_LEN, MAVLINK_MSG_ID_WIND_SENSOR_CRC);
+#endif
+}
+#endif
+
+#endif
+
+// MESSAGE WIND_SENSOR UNPACKING
+
+
+/**
+ * @brief Get field direction from wind_sensor message
+ *
+ * @return  Wind Direction
+ */
+static inline float mavlink_msg_wind_sensor_get_direction(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_float(msg,  0);
+}
+
+/**
+ * @brief Get field avg_mag from wind_sensor message
+ *
+ * @return  Average wind measured
+ */
+static inline float mavlink_msg_wind_sensor_get_avg_mag(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_float(msg,  4);
+}
+
+/**
+ * @brief Get field max_mag from wind_sensor message
+ *
+ * @return  Maximum wind measured
+ */
+static inline float mavlink_msg_wind_sensor_get_max_mag(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_float(msg,  8);
+}
+
+/**
+ * @brief Get field std_dev from wind_sensor message
+ *
+ * @return  Standard Deviation of measured wind magnitude
+ */
+static inline float mavlink_msg_wind_sensor_get_std_dev(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_float(msg,  12);
+}
+
+/**
+ * @brief Decode a wind_sensor message into a struct
+ *
+ * @param msg The message to decode
+ * @param wind_sensor C-struct to decode the message contents into
+ */
+static inline void mavlink_msg_wind_sensor_decode(const mavlink_message_t* msg, mavlink_wind_sensor_t* wind_sensor)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    wind_sensor->direction = mavlink_msg_wind_sensor_get_direction(msg);
+    wind_sensor->avg_mag = mavlink_msg_wind_sensor_get_avg_mag(msg);
+    wind_sensor->max_mag = mavlink_msg_wind_sensor_get_max_mag(msg);
+    wind_sensor->std_dev = mavlink_msg_wind_sensor_get_std_dev(msg);
+#else
+        uint8_t len = msg->len < MAVLINK_MSG_ID_WIND_SENSOR_LEN? msg->len : MAVLINK_MSG_ID_WIND_SENSOR_LEN;
+        memset(wind_sensor, 0, MAVLINK_MSG_ID_WIND_SENSOR_LEN);
+    memcpy(wind_sensor, _MAV_PAYLOAD(msg), len);
+#endif
+}
--- ASLUAV/testsuite.h
+++ ASLUAV/testsuite.h
@@ -793,6 +793,185 @@ static void mavlink_test_sens_power_board(uint8_t system_id, uint8_t component_i
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_wind_sensor(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_WIND_SENSOR >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_wind_sensor_t packet_in = {
+        17.0,45.0,73.0,101.0
+    };
+    mavlink_wind_sensor_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.direction = packet_in.direction;
+        packet1.avg_mag = packet_in.avg_mag;
+        packet1.max_mag = packet_in.max_mag;
+        packet1.std_dev = packet_in.std_dev;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_WIND_SENSOR_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_wind_sensor_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_wind_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_wind_sensor_pack(system_id, component_id, &msg , packet1.direction , packet1.avg_mag , packet1.max_mag , packet1.std_dev );
+    mavlink_msg_wind_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_wind_sensor_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.direction , packet1.avg_mag , packet1.max_mag , packet1.std_dev );
+    mavlink_msg_wind_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_wind_sensor_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_wind_sensor_send(MAVLINK_COMM_1 , packet1.direction , packet1.avg_mag , packet1.max_mag , packet1.std_dev );
+    mavlink_msg_wind_sensor_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_sens_4h_vector_probe(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_SENS_4H_VECTOR_PROBE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_sens_4h_vector_probe_t packet_in = {
+        93372036854775807ULL,73.0,101.0,129.0,157.0,185.0,213.0,241.0,269.0,297.0,325.0,353.0,381.0
+    };
+    mavlink_sens_4h_vector_probe_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.timestamp = packet_in.timestamp;
+        packet1.raw_pressure1 = packet_in.raw_pressure1;
+        packet1.raw_pressure2 = packet_in.raw_pressure2;
+        packet1.raw_pressure3 = packet_in.raw_pressure3;
+        packet1.raw_pressure4 = packet_in.raw_pressure4;
+        packet1.filtered_pressure1 = packet_in.filtered_pressure1;
+        packet1.filtered_pressure2 = packet_in.filtered_pressure2;
+        packet1.filtered_pressure3 = packet_in.filtered_pressure3;
+        packet1.filtered_pressure4 = packet_in.filtered_pressure4;
+        packet1.alpha = packet_in.alpha;
+        packet1.beta = packet_in.beta;
+        packet1.u = packet_in.u;
+        packet1.pt = packet_in.pt;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_SENS_4H_VECTOR_PROBE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_SENS_4H_VECTOR_PROBE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_sens_4h_vector_probe_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_sens_4h_vector_probe_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_sens_4h_vector_probe_pack(system_id, component_id, &msg , packet1.timestamp , packet1.raw_pressure1 , packet1.raw_pressure2 , packet1.raw_pressure3 , packet1.raw_pressure4 , packet1.filtered_pressure1 , packet1.filtered_pressure2 , packet1.filtered_pressure3 , packet1.filtered_pressure4 , packet1.alpha , packet1.beta , packet1.u , packet1.pt );
+    mavlink_msg_sens_4h_vector_probe_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_sens_4h_vector_probe_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.timestamp , packet1.raw_pressure1 , packet1.raw_pressure2 , packet1.raw_pressure3 , packet1.raw_pressure4 , packet1.filtered_pressure1 , packet1.filtered_pressure2 , packet1.filtered_pressure3 , packet1.filtered_pressure4 , packet1.alpha , packet1.beta , packet1.u , packet1.pt );
+    mavlink_msg_sens_4h_vector_probe_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_sens_4h_vector_probe_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_sens_4h_vector_probe_send(MAVLINK_COMM_1 , packet1.timestamp , packet1.raw_pressure1 , packet1.raw_pressure2 , packet1.raw_pressure3 , packet1.raw_pressure4 , packet1.filtered_pressure1 , packet1.filtered_pressure2 , packet1.filtered_pressure3 , packet1.filtered_pressure4 , packet1.alpha , packet1.beta , packet1.u , packet1.pt );
+    mavlink_msg_sens_4h_vector_probe_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_v4hp_calib_reference(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_V4HP_CALIB_REFERENCE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_v4hp_calib_reference_t packet_in = {
+        93372036854775807ULL,17651,17755
+    };
+    mavlink_v4hp_calib_reference_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.timestamp = packet_in.timestamp;
+        packet1.alpha = packet_in.alpha;
+        packet1.beta = packet_in.beta;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_V4HP_CALIB_REFERENCE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_V4HP_CALIB_REFERENCE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_v4hp_calib_reference_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_v4hp_calib_reference_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_v4hp_calib_reference_pack(system_id, component_id, &msg , packet1.timestamp , packet1.alpha , packet1.beta );
+    mavlink_msg_v4hp_calib_reference_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_v4hp_calib_reference_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.timestamp , packet1.alpha , packet1.beta );
+    mavlink_msg_v4hp_calib_reference_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_v4hp_calib_reference_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_v4hp_calib_reference_send(MAVLINK_COMM_1 , packet1.timestamp , packet1.alpha , packet1.beta );
+    mavlink_msg_v4hp_calib_reference_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ASLUAV(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_sens_power(system_id, component_id, last_msg);
@@ -807,6 +986,9 @@ static void mavlink_test_ASLUAV(uint8_t system_id, uint8_t component_id, mavlink
     mavlink_test_fw_soaring_data(system_id, component_id, last_msg);
     mavlink_test_sensorpod_status(system_id, component_id, last_msg);
     mavlink_test_sens_power_board(system_id, component_id, last_msg);
+    mavlink_test_wind_sensor(system_id, component_id, last_msg);
+    mavlink_test_sens_4h_vector_probe(system_id, component_id, last_msg);
+    mavlink_test_v4hp_calib_reference(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
